6b727d2bacc85b4d1868ed37892b869dfe89666b
Albert <97989034+albertpang@users.noreply.github.com>
2023-02-25 18:21:58 -0500

    rewrite for oop

--- AutoCAD.py
+++ AutoCAD.py
@@ -2,50 +2,36 @@ import win32com.client
 import numpy as np
 import pandas as pd
 from line import Line
+from fitting import Fitting
+
 acad = win32com.client.Dispatch("AutoCAD.Application")
 
 # iterate through all objects (entities) in the currently opened drawing
 # and if its a BlockReference, display its attributes.
 class Sheet:
     def __init__(self):
-        self.FittingsDF = pd.DataFrame(columns=['ID', 'Block Description', 
-                                                'Block X', 'Block Y'])
-        
+        self.fileName = 
         self.LinesDF = pd.DataFrame(columns=['ID', 'Block Description', 'Start X', 
-                                             'Start Y', 'End X', 'End Y'])
-    
+                                            'Start Y', 'End X', 'End Y'])
+        self.FittingsDF = pd.DataFrame(columns=['ID', 'Block Description', 
+                                        'Block X', 'Block Y'])
+        
     def findBlocks(self):
         for i, entity in enumerate(acad.ActiveDocument.Modelspace):
             print(i, entity)
-            if entity.ObjectName == 'AcDbLine':
-            # and entity.Layer == 'C-PR-WATER':
-                l = Line(entity)
-                self.LinesDF.loc[len(self.LinesDF.index)] = [l.ID, l.layer, l.startX,
-                                                            l.startY, l.endX, l.endY]
+            if entity.ObjectName == 'AcDbLine' and entity.Layer == 'C-PR-WATER':
+                l = Line(entity , self.LinesDF)
                                                             
-                
             if entity.ObjectName == 'AcDbBlockReference' and entity.EffectiveName.startswith("WATER"):
-                self.getFittingsProps(entity)
-        
+                f = Fitting(entity, self.FittingsDF)
 
-    def getFittingsProps(self, block):
-        FittingID = block.ObjectID
-        FittingsX = block.insertionPoint[0]
-        FittingsY = block.insertionPoint[1]
-        blockDynamicProperties = block.GetDynamicBlockProperties()
-        for prop in blockDynamicProperties:
-            if prop.PropertyName == 'Visibility1':
-                dynamicBlockName = prop.Value
-        self.FittingsDF.loc[len(self.FittingsDF.index)] = [FittingID, dynamicBlockName, FittingsX, FittingsY]
+        l.saveDF()
+        f.saveDF()
     
-    def saveDF(self):
-        self.FittingsDF.to_csv('FittingsCSV')
-        self.LinesDF.to_csv('LinesCSV')
-
 sheet = Sheet()
 sheet.findBlocks()
+
 # sheet.findFittings()
-sheet.saveDF()
 # sheet.findLines()
 
 
