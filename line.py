923bdad39ec59949cb8b801472ee447f9181c150
Albert <97989034+albertpang@users.noreply.github.com>
2023-02-25 09:51:38 -0500

    Block ID associated with blocks

--- line.py
+++ line.py
@@ -4,6 +4,7 @@ class Line:
         self._startY = block.StartPoint[1]
         self._endX = block.EndPoint[0]
         self._endY = block.EndPoint[1]
+        
     
     def getStart(self):
         return (self._startX, self._startY)
