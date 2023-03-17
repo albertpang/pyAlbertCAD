0932ad4cd95bb96d46056016ec01bb862d587f31
Albert <97989034+albertpang@users.noreply.github.com>
2023-03-17 03:15:47 -0400

    Doesn't Work

--- ACAD_DataTypes.py
+++ ACAD_DataTypes.py
@@ -1,7 +1,8 @@
+import time
 import win32com.client
 import pythoncom
 
-def APoint(x, y = 0, z = 0):
+def APoint(x, y, z = 0):
      return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, (x, y, z))
 
 
